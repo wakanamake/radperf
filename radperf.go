aaf4aa8957fed839fa2f2da30825a0dfa5777cd3
wakanamake <waka.namake@gmail.com>
2023-06-01 19:11:35 +0900

    add some features

--- radperf.go
+++ radperf.go
@@ -4,29 +4,34 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 	"net/netip"
 	"os"
 	"path/filepath"
+	"strconv"
+	"time"
 
 	"layeh.com/radius"
 	"layeh.com/radius/rfc2865"
 	"layeh.com/radius/rfc2866"
 )
 
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
 func main() {
 	var (
-		numPackets int
-		secret     string
-		ip         string
-		rate       int
-		destIP     string
-		destPort   int
-		username   string
-		sessionID  string
-		msisdn     string
-		start      bool
-		stop       bool
+		numPackets     int
+		secret         string
+		ip             string
+		rate           int
+		destIP         string
+		destPort       int
+		usernamePrefix string
+		msisdn         int
+		start          bool
+		stop           bool
+		servicePlan    string
 	)
 
 	// Define the flags
@@ -35,10 +40,10 @@ func main() {
 	flag.StringVar(&ip, "i", "192.168.0.1", "Client IP address with prefix size (CIDR notation)")
 	flag.IntVar(&rate, "r", 1, "Rate of packet transmission (packets per second)")
 	flag.StringVar(&destIP, "d", "127.0.0.1", "Destination IP address")
+	flag.StringVar(&servicePlan, "sp", "Default Service Plan", "Service Plan name")
 	flag.IntVar(&destPort, "p", 1813, "Destination UDP port")
-	flag.StringVar(&msisdn, "m", "01012345678", "MSISDN value")
-	flag.StringVar(&username, "user", "HOGE", "Username prefix")
-	flag.StringVar(&sessionID, "sid", "0123", "Session ID prefix")
+	flag.IntVar(&msisdn, "m", 12345678, "MSISDN value")
+	flag.StringVar(&usernamePrefix, "user", "HOGE", "Username prefix")
 	flag.BoolVar(&start, "start", false, "Allow to send Accounting START message")
 	flag.BoolVar(&stop, "stop", false, "Allow to send Accounting STOP message")
 
@@ -57,6 +62,11 @@ func main() {
 	// Show usage if no arguments are provided
 	if flag.NFlag() == 0 {
 		flag.Usage()
+		return
+	}
+
+	if !start && !stop {
+		fmt.Println("Error: Either 'start' or 'stop' option must be specified.")
 		return
 	}
 
@@ -89,27 +99,54 @@ func main() {
 	}
 	defer conn.Close()
 
+	id := 0
+	sessionIDPrefix := GenerateRandomString(6)
+
 	for i := 0; i < numPackets; i++ {
 		packet := radius.New(radius.CodeAccountingRequest, []byte(secret))
-
+		username := fmt.Sprintf("%s%08d", usernamePrefix, id)
+		sessionID := fmt.Sprintf("%s%08d", sessionIDPrefix, id)
 		// Attributes
-		rfc2865.UserName_SetString(packet, "HOGE")
-		rfc2866.AcctSessionID_SetString(packet, "1234567890")
-		rfc2866.AcctStatusType_Add(packet, rfc2866.AcctStatusType_Value_Start)
+		rfc2865.UserName_SetString(packet, username)
+		rfc2866.AcctSessionID_SetString(packet, sessionID)
 		rfc2865.FramedIPAddress_Add(packet, net.IP(startIP.AsSlice()))
-		rfc2865.CallingStationID_SetString(packet, msisdn)
+		rfc2865.CallingStationID_SetString(packet, strconv.Itoa(msisdn))
+		rfc2865.Class_SetString(packet, servicePlan)
 
-		encodedPacket, err := packet.Encode()
-		if err != nil {
-			log.Fatalf("Error encoding packet: %v", err)
+		if start {
+			rfc2866.AcctStatusType_Add(packet, rfc2866.AcctStatusType_Value_Start)
+		} else if stop {
+			rfc2866.AcctStatusType_Add(packet, rfc2866.AcctStatusType_Value_Stop)
 		}
-		// Write the packet to the RADIUS server.
-		_, err = conn.Write(encodedPacket)
-		if err != nil {
-			log.Fatalf("Error writing: %v", err)
-		}
+		sendUDPPacket(conn, packet)
+
+		id++
+		msisdn++
 		startIP = startIP.Next()
 	}
 
 	fmt.Println("RADIUS Accounting Start messages sent.")
+}
+
+func GenerateRandomString(n int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[r.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func sendUDPPacket(conn *net.UDPConn, packet *radius.Packet) {
+	// Encode the pakcet
+	encodedPacket, err := packet.Encode()
+	if err != nil {
+		log.Fatalf("Error encoding packet: %v", err)
+	}
+	// Write the packet to the RADIUS server.
+	_, err = conn.Write(encodedPacket)
+	if err != nil {
+		log.Fatalf("Error writing: %v", err)
+	}
 }
